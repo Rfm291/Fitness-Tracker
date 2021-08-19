6b15d9e29959be63a135fca0c3720f9a81398be0
Richard Mercado <RichardFM291@gmail.com>
2021-08-19 13:07:41 -0400

    modified server and api

--- models/Workout.js
+++ models/Workout.js
@@ -4,7 +4,38 @@ const Schema = mongoose.Schema;
 
 const WorkoutSchema = new Schema({
   day: Date,
-  exercises: []
+  exercises: [
+    {
+      type: {
+        type: String,
+        required: 'Enter type of exercise to perform',
+      },
+      name: {
+        type: String,
+        required: 'Enter exercise name to perform',
+      },
+      duration: {
+        type: Number,
+        required: 'Enter duration in minutes',
+      },
+      distance: {
+        type: Number
+      },
+      weight: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+    },
+  ],
+  day: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 const Workout = mongoose.model("Workout", WorkoutSchema);
--- server.js
+++ server.js
@@ -15,51 +15,16 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { 
+useNewUrlParser: true,
+useUnifiedTopology: true,
+useCreateIndex: true,
+useFindAndModify: false
+ });
 
-
-
-// app.post("/submit", ({body}, res) => {
-//   db.Book.create(body)
-//     .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-//     .then(dbLibrary => {
-//       res.json(dbLibrary);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/books", (req, res) => {
-//   db.Book.find({})
-//     .then(dbBook => {
-//       res.json(dbBook);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/library", (req, res) => {
-//   db.Library.find({})
-//     .then(dbLibrary => {
-//       res.json(dbLibrary);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-// app.get("/populated", (req, res) => {
-//   db.Library.find({})
-//     .populate("books")
-//     .then(dbLibrary => {
-//       res.json(dbLibrary);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
+ //routes
+ app.use(require("./routes/api"));
+ app.use(require("./routes/htmlRoutes"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
